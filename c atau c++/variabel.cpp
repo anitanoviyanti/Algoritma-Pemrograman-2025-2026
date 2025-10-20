d93b6afa5767ef98932156bbd17b864cee99d0fb
anitanoviyanti <anitanoviyanti44@gmail.com>
2025-10-20 11:16:38 +0700

    LATIHAN TIPE DATA, VARIABEL, & KONSTANTA C++
    Mengerjakan latihan soal Tipe Data, Variabel, dan Konstanta.

---
+++ c atau c++/variabel.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+main()
+{
+    short a;
+    int b;
+    long c;
+    float d;
+    double e;
+    char data_karakter = 'A';
+    char materi1[9] = "Variabel";
+
+    a = 10000;
+    c = 1000000;
+    d = 1000000000;
+    e = 3.12;
+    
+    cout << "---BELAJAR VARIABEL---" << endl;
+    cout << "Variabel a :" << a << endl;
+    cout << "Variabel b :" << b << endl;
+    cout << "Variabel c :" << c << endl;
+    cout << "Variabel d :" << d << endl;
+    cout << "Variabel e :" << e << endl;
+    cout << "Variabel data_karakter :" << data_karakter << endl;
+    cout << "Variabel materi1 :" << materi1 << endl;
+    cout << endl;
+}
