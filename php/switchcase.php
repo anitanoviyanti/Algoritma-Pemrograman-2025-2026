ac5c8ac932e3aae9d3b9432907dad88aa2198996
anitanoviyanti <anitanoviyanti44@gmail.com>
2026-01-21 21:04:49 +0700

    Percabangan IF & Switch Case
    Tugas Forum Diskusi 01 - 02 - 26

---
+++ php/switchcase.php
@@ -0,0 +1,17 @@
+<?php
+$hari = "Rabu";
+
+switch($hari) {
+    case "Senin":
+        echo "Hari ini adalah Senin.";
+        break;
+    case "Selasa":
+        echo "Hari ini adalah Selasa.";
+        break;
+    case "Rabu":
+        echo "Hari ini adalah Rabu.";
+        break;
+    default:
+        echo "Hari tidak dikenal.";
+}
+?>
---
+++ python/percabanganIF.py
@@ -0,0 +1,6 @@
+umur = int(input("Masukkan umur Anda : "))
+
+if umur >= 18:
+    print("Anda sudah dewasa.")
+else:
+    print("Anda masih anak-anak.")
