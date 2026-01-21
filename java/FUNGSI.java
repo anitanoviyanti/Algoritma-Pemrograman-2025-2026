2cdc90abb9fb4c7c5fa0a83050f992677cf566ea
anitanoviyanti <anitanoviyanti44@gmail.com>
2026-01-21 22:30:00 +0700

    Prosedur Fungsi Tugas Diskusi
    Prosedur dan Fungsi Menggunakan Bahasa Pemrograman Java

---
+++ java/FUNGSI.java
@@ -0,0 +1,12 @@
+public class FUNGSI {
+
+
+    static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(5, 3);
+        System.out.println("Hasil penjumlahan: " + hasil);
+    }
+}
---
+++ java/PROSEDUR.java
@@ -0,0 +1,11 @@
+public class PROSEDUR {
+
+    
+    static void tampilNama() {
+        System.out.println("Nama saya Anita");
+    }
+
+    public static void main(String[] args) {
+        tampilNama();
+    }
+}
